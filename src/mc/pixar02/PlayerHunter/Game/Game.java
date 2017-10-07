2cd574622340a19661743a8a0cfe2de97c6c5970
pixar02 <pixar.pf@gmail.com>
2017-10-07 16:22:41 +0200

    becomming playable...

--- src/mc/pixar02/PlayerHunter/Game/Game.java
+++ src/mc/pixar02/PlayerHunter/Game/Game.java
@@ -1,14 +1,32 @@
 package mc.pixar02.PlayerHunter.Game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Location;
+import org.bukkit.World;
+
+import mc.pixar02.PlayerHunter.PlayerHunter;
 
 public class Game {
+	private PlayerHunter plugin;
+	private String mapName;
+	private World mapWorld;
+	private GameMap gameMap;
+	private Location specSpawn;
+	private Location joinSpawn;
+	private List<GamePlayer> gPlayers = new ArrayList<GamePlayer>();
+	private List<GamePlayer> spectators = new ArrayList<GamePlayer>();
+	private Map<GamePlayer, Integer> kills = new HashMap<GamePlayer, Integer>();
+	private int gameNumb;
+	private int minPlayers;
 
-	private Location SpecSpawn;
-	private Location JoinSpawn;
-
-	public Game() {
-
+	public Game(PlayerHunter plugin, int gameNumb, String map) {
+		this.plugin = plugin;
+		this.gameNumb = gameNumb;
+		this.mapName = map;
 	}
 
 	public void startGame() {
@@ -17,5 +35,17 @@ public class Game {
 
 	public void endGame() {
 
+	}
+
+	public String getMapName() {
+		return mapName;
+	}
+
+	public Location getSpecSpawn() {
+		return specSpawn;
+	}
+
+	public World getMapWorld() {
+		return mapWorld;
 	}
 }
